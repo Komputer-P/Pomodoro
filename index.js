9973301cc95a630c382d94e8b386edbd4195bf38
Komputer-P <wlghks0314@gmail.com>
2021-04-07 16:00:34 +0900

    Timer Set Option Added

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const timer_display = document.querySelector('.timer_display');
 const start_timer_button = document.querySelector('.start_timer_button');
 const pomodoro_count_display = document.querySelector('.pomodoro_count');
+const timer_set_form = document.querySelector('.timer_set_form');
 
 let countdown;
 let pomo = 0;
+let time_set = 25 * 60;
 
 //FUNCTIONS
 
@@ -44,10 +46,29 @@ function update_pomo() {
 }
 
 function timer_init() {
-    let set_time_seconds = 25 * 60;
+    display_time_left(time_set);
+}
 
-    timer(set_time_seconds);
+function timer_start() {
+    timer(time_set);
+}
+
+function timer_set(e) {
+    e.preventDefault();
+    
+    const time_string = this.querySelector('.timer_set_text').value;
+    this.querySelector('.timer_set_text').value = "";
+
+    const time_parsed = time_string.split(':');
+    const minutes = parseInt(time_parsed[0]);
+    const seconds = parseInt(time_parsed[1]);
+    console.log(minutes);
+    time_set = minutes * 60 + seconds;
+
+    timer_init();
 }
 
 //INIT
-start_timer_button.addEventListener('click', timer_init);
+start_timer_button.addEventListener('click', timer_start);
+timer_set_form.addEventListener('submit',timer_set);
+timer_init();
